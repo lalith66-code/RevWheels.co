e885b4edd4fc48d2a11f0fffefb22f9f535d867e
lalith66-code <beingwithnature5253@gmail.com>
2026-01-04 00:16:30 +0530

    edited

--- final/static/js/admin.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".delete-btn").forEach(btn => {
-    btn.addEventListener("click", (e) => {
-      if (!confirm("Delete this product?")) {
-        e.preventDefault();
-      }
-    });
-  });
-});
